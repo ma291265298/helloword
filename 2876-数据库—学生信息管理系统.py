1ecf2c8f233834eb556f03bac6c61c75e693f053
Steven H <291265298@qq.com>
2019-04-26 19:15:13 +0800

    python学生信息系统

--- 2876-数据库—学生信息管理系统.py
+++ 2876-数据库—学生信息管理系统.py
@@ -15,48 +15,84 @@ column_names = ['学号', '姓名', '性别', '数学', '英语', '计算机']
 
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title="学生信息管理系统", size=(700, 500))
+        super().__init__(parent=None, title="学生信息管理系统", size=(700, 550))
         self.Center()
         panel = wx.Panel(self)
-        box1 = wx.BoxSizer(wx.VERTICAL)
-        box2 = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.VERTICAL)
+        box1 = wx.BoxSizer(wx.HORIZONTAL)
+        box2 = wx.BoxSizer(wx.HORIZONTAL)
         box3 = wx.BoxSizer(wx.HORIZONTAL)
-        self.grid = self.CreateGrid(self)
-        tb1=wx.TextCtrl()
-        box2.Add(self.grid, flag=wx.ALIGN_CENTER | wx.ALL | wx.EXPAND)
-        panel.SetSizer(box2)
 
+        st1 = wx.StaticText(panel, label='请输入姓名：')
+        self.tc1 = wx.TextCtrl(panel)
+        bt1 = wx.Button(panel, label='查询', id=1)
+        box1.Add(st1, flag=wx.ALIGN_CENTER | wx.ALL, border=20)
+        box1.Add(self.tc1, flag=wx.ALIGN_CENTER | wx.ALL, border=15)
+        box1.Add(bt1, flag=wx.ALIGN_CENTER | wx.ALL, border=15)
+
+        self.grid = self.CreateGrid(panel)
+        box2.Add(self.grid, flag=wx.ALIGN_CENTER | wx.ALL)
+
+        add = wx.Button(panel, label='添加')
+        edit = wx.Button(panel, label='修改')
+        delete = wx.Button(panel, label='删除')
+        box3.Add(add, flag=wx.ALIGN_CENTER | wx.ALL, proportion=1)
+        box3.Add(edit, flag=wx.ALIGN_CENTER | wx.ALL, proportion=1)
+        box3.Add(delete, flag=wx.ALIGN_CENTER | wx.ALL, proportion=1)
+
+        hbox.Add(box1, flag=wx.ALIGN_CENTER, proportion=1)
+        hbox.Add(box2, flag=wx.ALIGN_CENTER | wx.BOTTOM, proportion=1)
+        hbox.Add(box3, flag=wx.ALIGN_CENTER | wx.BOTTOM, proportion=10)
+
+        panel.SetSizer(hbox)
+
+        self.statusbar = self.CreateStatusBar()
+        self.statusbar.SetStatusText('准备就绪')
         # panel.Layout()
-        self.Bind(wx.grid.EVT_GRID_CMD_SELECT_CELL, self.OnClick, self.grid)
+        # self.Bind(wx.grid.EVT_GRID_CMD_SELECT_CELL, self.OnClick, self.grid)
+        self.Bind(wx.EVT_BUTTON, self.select, id=1)
 
-    def OnClick(self, event):
-        print("行号:{0}".format(event.GetRow()))
-        print("列号:{0}".format(event.GetCol()))
-        print(data[event.GetRow()])
-        event.Skip()
+    def select(self, event):
+        name = self.tc1.GetValue()
+        try:
+            with conn.cursor() as cursor:
+                sql = "select * from student where name='{}'".format(name)
+                print(sql)
+                cursor.execute(sql)
+                conn.commit()
+                print(cursor.fetchall())
+                data = cursor.fetchall()
+                data = list(data)
+                self.grid.Refresh()
+                print(data)
+                print('查询成功')
+        except pymysql.DatabaseError:
+            print('查询失败')
+            conn.rollback()
 
     def CreateGrid(self, parent):
         grid = wx.grid.Grid(parent)
         grid.CreateGrid(len(data), len(data[0]))
-        grid.SetColSize(0, 60)
-        grid.SetColSize(1, 100)
-        grid.SetColSize(2, 60)
-        grid.SetColSize(3, 100)
-        grid.SetColSize(4, 100)
-        grid.SetColSize(5, 100)
-        grid.SetColMinimalAcceptableWidth(60)
+        grid.SetColSize(0, 70)
+        grid.SetColSize(1, 110)
+        grid.SetColSize(2, 70)
+        grid.SetColSize(3, 110)
+        grid.SetColSize(4, 110)
+        grid.SetColSize(5, 110)
+        grid.SetColMinimalAcceptableWidth(80)
+        grid.SetRowMinimalAcceptableHeight(50)
         for r in range(len(data)):
+            grid.SetRowSize(r, 50)
             for c in range(len(data[r])):
-                # grid.SetRowAttr()
-                # grid.SetColMinimalAcceptableWidth(50)
                 grid.SetColLabelValue(c, column_names[c])
                 grid.SetCellValue(r, c, str(data[r][c]))
                 grid.SetCellAlignment(r, c, wx.ALIGN_CENTRE, wx.ALIGN_CENTRE)
-                # font = wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD, False)
-                # grid.SetCellFont(r, c, font)
-        # grid.SetDefaultColSize(50)
+                font = wx.Font(15, wx.SWISS, wx.NORMAL, wx.BOLD, False)
+                grid.SetCellFont(r, c, font)
         grid.AutoSize()
         return grid
+
+
 
 
 class App(wx.App):
@@ -77,14 +113,16 @@ if __name__ == '__main__':
 
             cursor.execute(sql)
             conn.commit()
+
             data = cursor.fetchall()
             data = list(data)
             print(data)
             print('查询成功')
+            app = App()
+            app.MainLoop()
     except pymysql.DatabaseError:
         print('查询失败')
         conn.rollback()
     finally:
         conn.close()
-    app = App()
-    app.MainLoop()
+
